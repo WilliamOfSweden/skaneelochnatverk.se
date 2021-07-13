28d51327c5ecf077852c398eb1a6aafb436f2d92
William Daghouz <w.daghouz@gmail.com>
2021-07-13 20:48:20 +0200

    Added gatsby-plugin & data to gatsby-config file & created an images folder and a content folder.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,59 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+    siteMetadata: {
+        ogDescription: `Vi utför el- och nätverksarbete i södra Skåne.`,
+        ogImageUrl: `https://skaneelochnatverk.se/banner.png`,
+        ogType: 'website',
+        siteDescription: `Company Websites | Skåne El & Nätverk`,
+        siteTitle: `Skåne El & Nätverk`,
+        siteUrl: `https://skaneelochnatverk.se`,
+        navLinks:[
+            {
+                name: 'Tjänster',
+                link: 'services',
+            },
+            {
+                name: 'Certifikat & Referenser',
+                link: 'references',
+            },
+            {
+                name: 'Om oss',
+                link: 'about',
+            },
+            {
+                name: 'Kontakt',
+                link: 'contact',
+            },
+        ],
+    },
+    plugins: [
+        `gatsby-plugin-styled-components`,
+        {
+            resolve: `gatsby-plugin-material-ui`,
+            options: {
+                stylesProvider: {
+                    injectFirst: true,
+                },
+            },
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/images/`,
+            },
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+              name: `content`,
+              path: `${__dirname}/content/`,
+            },
+        },
+        `gatsby-plugin-mdx`,
+        `gatsby-plugin-image`,
+        `gatsby-plugin-sharp`,
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-react-helmet`,
+    ],
 }
+  
