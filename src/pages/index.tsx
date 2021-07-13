f42c0f32d8e6abe2ad8bfdb5f80a7039787615a6
William Daghouz <w.daghouz@gmail.com>
2021-07-13 20:34:48 +0200

    Replaced favicon and created a logo.svg file.

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import React, { FC } from 'react'
+import { PageProps } from 'gatsby'
+
+
+const IndexPage: FC<PageProps> = () => {
+    
+    return (
+    
+        <div>
+      
+        </div>
+
+    )
+
+}
+
+
+export default IndexPage
