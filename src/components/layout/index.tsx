8cd2040f75b3a7ad1b89b9b7c6c3205c66926f4f
William Daghouz <w.daghouz@gmail.com>
2021-07-13 20:58:25 +0200

    Created a global MDX provider.

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,7 +1,9 @@
 import React, { FC, ReactNode } from 'react'
+import Typography from '@material-ui/core/Typography'
 import { ThemeProvider } from '@material-ui/styles'
 import theme from '../../styles/theme'
 import CssBaseline from '@material-ui/core/CssBaseline'
+import { MDXProvider } from '@mdx-js/react'
 
 
 interface Props {
@@ -9,6 +11,20 @@ interface Props {
     children: ReactNode       
 
 }
+
+const MDXh1: FC<Props> = ({ children }) => <Typography variant='h1'>{ children }</Typography>
+
+
+const MDXh2: FC<Props> = ({ children }) => <Typography variant='h2'>{ children }</Typography>
+
+
+const MDXh3: FC<Props> = ({ children }) => <Typography variant='h3'>{ children }</Typography>
+
+
+const MDXh4: FC<Props> = ({ children }) => <Typography variant='h4'>{ children }</Typography>
+
+
+const MDXparagraph: FC<Props> = ({ children }) => <Typography component='p' variant='body1'>{ children }</Typography>
 
 
 const Layout: FC<Props> = ({ children }) => {
@@ -19,11 +35,23 @@ const Layout: FC<Props> = ({ children }) => {
 
             <CssBaseline />
         
-            <main>
+            <MDXProvider
+                components={{
+                    h1: MDXh1,
+                    h2: MDXh2,
+                    h3: MDXh3,
+                    h4: MDXh4,
+                    p: MDXparagraph,
+                }}
+            >
 
-                { children }
-                
-            </main>
+                <main>
+
+                    { children }
+
+                </main>
+
+            </MDXProvider>
 
         </ThemeProvider>
     
