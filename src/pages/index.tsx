424ba21ded4892e0ad32c75f3a154c3124a1d949
William Daghouz <w.daghouz@gmail.com>
2021-07-15 07:03:58 +0200

    Temporarily commented out animated svg illustration to track small performance isse.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,9 @@ import { PageProps } from 'gatsby'
 import Layout from '../components/layout/'
 import Masthead from '../components/indexPage/masthead/'
 import TrustedBySection from '../components/indexPage/trustedBySection/'
-import AnimatedUpperDivider from '../components/indexPage/animatedUpperDivider'
-import AnimatedMiddleDivider from '../components/indexPage/animatedMiddleDivider'
-import AnimatedLowerDivider from '../components/indexPage/animatedLowerDivider'
+// import AnimatedUpperDivider from '../components/indexPage/animatedUpperDivider'
+// import AnimatedMiddleDivider from '../components/indexPage/animatedMiddleDivider'
+// import AnimatedLowerDivider from '../components/indexPage/animatedLowerDivider'
 import CTASection from '../components/indexPage/ctaSection/'
 import ServicesSection from '../components/indexPage/servicesSection/'
 import EnviromentSection from '../components/indexPage/environmentSection/'
@@ -27,15 +27,15 @@ const IndexPage: FC<PageProps> = () => {
 
                 <TrustedBySection />
 
-                <AnimatedUpperDivider />
+                {/* <AnimatedUpperDivider /> */}
 
                 <CTASection />
 
-                <AnimatedMiddleDivider />
+                {/* <AnimatedMiddleDivider /> */}
 
                 <ServicesSection />
 
-                <AnimatedLowerDivider />
+                {/* <AnimatedLowerDivider /> */}
 
                 <EnviromentSection />
 
