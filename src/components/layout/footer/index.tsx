04f76a20dd8330714349344520224028d79dc9ae
William Daghouz <w.daghouz@gmail.com>
2021-10-21 19:24:42 +0200

    Restored company adress.

--- src/components/layout/footer/index.tsx
+++ src/components/layout/footer/index.tsx
@@ -93,8 +93,8 @@ const Footer = () => {
               <BusinessIcon className={classes.icon} />
               <Typography>
                 Skåne El &amp; Nätverk AB <br />
-                Knutsborg 22 A <br />
-                245 45 Staffanstorp
+                Traversvägen 1 <br />
+                245 34 Staffanstorp
               </Typography>
             </Box>
             <Box display='flex' mb={2}>
